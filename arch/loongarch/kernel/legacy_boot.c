f2a8d5715edfba3432aceaa26af5f826267ef80e
Elysia <a.elysia@proton.me>
2026-01-16 20:32:37 +0800

    AOSCOS: LoongArch: Add ACPI compatibility for ISA I/O space reservation
    This patch adds essential compatibility support for ACPI-based systems running on older firmware. Many older computers with old firmware still rely on this mechanism and are unlikely to receive futher firmeware updates. This change ensures proper ISA I/O space reservation across for them.
    
    Use DMI to judge if the system needs patching.
    
    Signed-off-by: Yinan Qin <a.elysia@proton.me>

--- arch/loongarch/kernel/legacy_boot.c
+++ arch/loongarch/kernel/legacy_boot.c
@@ -1,4 +1,8 @@
 #include <linux/efi.h>
+#include <linux/dmi.h>
+#include <linux/ioport.h>
+#include <linux/logic_pio.h>
+#include <linux/sizes.h>
 #include <linux/memblock.h>
 #include <linux/acpi.h>
 #include <linux/kmemleak.h>
@@ -87,6 +91,10 @@ static enum bpi_vers bpi_version = BPI_VERSION_NONE;
 static u64 __initdata bpi_flags = 0;
 
 static int have_bpi = 0;
+
+static void __iomem *legacy_isa_base;
+static struct resource *legacy_isa_res;
+static struct fwnode_handle *legacy_isa_fwnode;
 
 static __initdata struct {
 	size_t map_count;
@@ -718,3 +726,68 @@ void __init acpi_arch_init (){
 int loongarch_have_legacy_bpi (void){
 	return have_bpi;
 }
+
+/* DMI matching table for hardware requiring legacy ISA support */
+static const struct dmi_system_id loongarch_legacy_isa_table[] = {
+    {
+        .ident = "Seewo CB.L3A6.MA01",
+        .matches = {
+            DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "CB.L3A6.MA01"),
+        },
+    },
+    { }
+};
+
+static int __init add_legacy_isa_io(struct fwnode_handle *fwnode,
+				resource_size_t hw_start, resource_size_t size)
+{
+	int ret = 0;
+	unsigned long vaddr;
+	struct logic_pio_hwaddr *range;
+
+	range = kzalloc(sizeof(*range), GFP_ATOMIC);
+	if (!range)
+		return -ENOMEM;
+
+	range->fwnode = fwnode;
+	range->size = size = round_up(size, PAGE_SIZE);
+	range->hw_start = hw_start;
+	range->flags = LOGIC_PIO_CPU_MMIO;
+
+	ret = logic_pio_register_range(range);
+	if (ret) {
+		kfree(range);
+		return ret;
+	}
+
+	/* Legacy ISA must placed at the start of PCI_IOBASE */
+	if (range->io_start != 0) {
+		logic_pio_unregister_range(range);
+		kfree(range);
+		return -EINVAL;
+	}
+
+	vaddr = (unsigned long)(PCI_IOBASE + range->io_start);
+	vmap_page_range(vaddr, vaddr + size, hw_start, pgprot_device(PAGE_KERNEL));
+
+	return 0;
+}
+
+static __init int loongarch_reserve_pio_range(void)
+{
+	struct fwnode_handle *fwnode;
+
+	if (!acpi_disabled && dmi_check_system(loongarch_legacy_isa_table)) {
+		fwnode = acpi_alloc_fwnode_static();
+		pr_info("Legacy ISA: Detected Legacy hardware (%s), setting up legacy ISA I/O\n",
+				dmi_get_system_info(DMI_PRODUCT_NAME));
+		if (add_legacy_isa_io(fwnode, LOONGSON_LIO_BASE, SZ_64K)) {
+			pr_warn("Legacy ISA: Failed to setup legacy ISA I/O, some devices may not work!\n");
+			acpi_free_fwnode_static(fwnode);
+		}
+	}
+
+	return 0;
+}
+
+arch_initcall(loongarch_reserve_pio_range);
